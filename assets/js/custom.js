4b2ed406e532b3882f5bb93a9d4a2e801ab78a5d
Belal Khan <belalkhan.dev@gmail.com>
2020-04-30 04:10:33 +0600

    tabnews paper and opinion section add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,6 @@
     jQuery(document).ready(function () {
         
         //feature news owl-carousel
-        console.log($('.featured-news-item').length);
         if ($('.featured-news-item').length > 0) {
             $('#featureNewsCarousel').owlCarousel({
                 items: 4,
@@ -13,6 +12,20 @@
             });
         }
 
+
+
+    });
+
+    /*==========   Sticky Navbar   ==========*/
+    $(window).on('scroll', function () {
+        if ($(window).width() >= 992) {
+            var $navbar = $('.sticky-navbar');
+            if ($(window).scrollTop() > 100) {
+                $navbar.addClass('fixed-navbar');
+            } else {
+                $navbar.removeClass('fixed-navbar');
+            }
+        }
     });
 
 
